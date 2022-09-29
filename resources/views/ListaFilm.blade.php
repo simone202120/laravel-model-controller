8ae711674a3f38a525e6198d72536646f8c8e993
Simone Fortunato <frtsmn94@gmail.com>
2022-09-29 17:44:29 +0200

    scss

--- resources/views/ListaFilm.blade.php
+++ resources/views/ListaFilm.blade.php
@@ -5,14 +5,13 @@
 @section('content')
 <h1>Lista Film:</h1>
     @foreach($films as $film)
-        <div class="titolo film">
-
+    <div class="container">
+        <div class="filmCard">
+            <h3 class="titolo">titolo: {{$film->title}}</h3>
+            <p class="nazionalità">nazionalità: {{$film->nationality}}</p>
+            <p class="data">data di uscita:{{$film->date}}</p>
+            <p class="voto">voto:{{$film->vote}}</p>
         </div>
-        <div class="nazionalità">
-
-        </div>
-        <div class="data uscita">
-            
-        </div>
+    </div>
     @endforeach
 @endsection

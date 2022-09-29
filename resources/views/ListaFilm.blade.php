37f5ebf335aba30ecb12ac419d41c3be8cb714d6
Simone Fortunato <frtsmn94@gmail.com>
2022-09-29 18:03:16 +0200

    Fine esercizio

--- resources/views/ListaFilm.blade.php
+++ resources/views/ListaFilm.blade.php
@@ -4,14 +4,16 @@
 
 @section('content')
 <h1>Lista Film:</h1>
-    @foreach($films as $film)
+
     <div class="container">
-        <div class="filmCard">
-            <h3 class="titolo">titolo: {{$film->title}}</h3>
-            <p class="nazionalità">nazionalità: {{$film->nationality}}</p>
-            <p class="data">data di uscita:{{$film->date}}</p>
-            <p class="voto">voto:{{$film->vote}}</p>
-        </div>
+        @foreach($films as $film)
+            <div class="filmCard">
+                <h3 class="titolo">titolo: {{$film->title}}</h3>
+                <p class="nazionalità">nazionalità: {{$film->nationality}}</p>
+                <p class="data">data di uscita:{{$film->date}}</p>
+                <p class="voto">voto:{{$film->vote}}</p>
+            </div>
+        @endforeach
     </div>
-    @endforeach
+
 @endsection
